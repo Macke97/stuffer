4e941c8e922d65188bd48b600ec13a83a8f4c1ca
Marcus Thelin <marcusthelin97@gmail.com>
2019-03-11 13:49:49 +0100

    Removed reject from stuffer file.

--- services/stuffer/ssr/extensions/dev/mercedes/index.js
+++ services/stuffer/ssr/extensions/dev/mercedes/index.js
@@ -8,19 +8,21 @@ import Excel2JSON from './Excel2JSON.class'
 import { twentyFour, thirtySix, fourtyEight, sixty } from './worksheets'
 import { REGISTER_PROCESSORS } from 'features/postprocess/hooks'
 const fileName = file => `${file.fileName}.json`
-const exec = (origin, target, task) => new Promise(async (resolve, reject) => {
+const exec = (origin, target, task) => new Promise(async (resolve) => {
     const workbook = xlsx.readFile(origin, { sheetStubs: true });
     const excel2JSON = new Excel2JSON(workbook, [twentyFour, thirtySix, fourtyEight, sixty])
     let data;
     try {
         data = await excel2JSON.start()
     } catch (error) {
-        console.error("\x1b[31m", error.message)
-        reject()
+        console.log(error)
+        resolve()
+        return
     }
     await fs.writeJSON(target, data)
 
     resolve()
+    console.log('Excel conversion done!')
 
 })
 export default ({ registerAction }) =>
